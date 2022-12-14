28daf15a4efa35653d5b9a481c816a8eb8eac9c3
Dharmil Desai <42259391+casterix193@users.noreply.github.com>
2022-12-14 12:29:45 +0200

    remove clutter

--- src/components/old/pages/Articles.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Articles() {
-    return <h1 className='articles'>ARTICLES</h1>
-}
--- src/components/old/pages/Resources.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Resources() {
-    return <h1 className='resources'>RESOURCES</h1>
-}
